5fc5ea19d84cd5a353e32c520c8b26c8230e8b23
FrankFonts <9556806+FrankFonts@users.noreply.github.com>
2022-05-11 14:11:29 +0200

    Exercise done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import faker from '@faker-js/faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'typing-game';
+  str: string = faker.lorem.sentence();
+  correct: string[] = [];
+  wrong: string[] = [];
+  unchecked: string[] = this.str.split('');
+
+  youWon: boolean = false;
+
+  atChange(typedString: any) {
+    let isCorrectLetter = true;
+    this.correct = [];
+    this.wrong = [];
+    //
+    for (let i = 0; i < typedString.length; i++) {
+      if (typedString[i] === this.str[i] && isCorrectLetter) {
+        this.correct.push(this.str[i]);
+      } else {
+        isCorrectLetter = false;
+        this.wrong.push(this.str[i]);
+      }
+    }
+    //
+    const x = this.str.length - this.correct.length - this.wrong.length;
+    if (x >= 0) {
+      this.unchecked = this.str.split('').splice(this.str.length - x, x);
+    }
+    //
+    if (typedString === this.str) {
+      this.youWon = true;
+    }
+  }
 }
